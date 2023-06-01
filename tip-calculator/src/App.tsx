89fce56041cbbedec30b36aec6ddbceaa9d07ed7
Joe Glanville <josephglanville@gmail.com>
2023-06-01 17:10:27 +0100

    Add AmountOutput component

--- tip-calculator/src/App.tsx
+++ tip-calculator/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import AmountOutput from './components/AmountOutput';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <AmountOutput label="Tip Amount" value={10.1} />
+      <AmountOutput label="Total" value={20.836} />
+    </div>
+  );
 };
 
 export default App;
