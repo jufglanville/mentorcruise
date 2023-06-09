071ec849c092412e02087f095283d35797a41eb6
Joe Glanville <josephglanville@gmail.com>
2023-06-09 17:25:46 +0100

    Initial commit, env setup

---
+++ form-class-based/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.tsx',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+	transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.svg$'
+  ],
+  moduleNameMapper: {
+    '\\.svg$': '<rootDir>/src/__mocks__/svgMock.ts'
+  }
+}
