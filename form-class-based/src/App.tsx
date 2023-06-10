f95315d0fcc764007878b6659ff9e939ffcd84f7
Joe Glanville <josephglanville@gmail.com>
2023-06-10 09:24:47 +0100

    Update App.tsx to class based component

--- form-class-based/src/App.tsx
+++ form-class-based/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './components/Button';
 
-const App = () => {
-  return (
-    <div>
-      <Button active={true} onClick={() => console.log('Clicked')}>
-        Claim your free trial
-      </Button>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button active={true} onClick={() => console.log('Clicked')}>
+          Claim your free trial
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default App;
